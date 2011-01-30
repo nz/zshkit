68947e7935670b0017b4af047a3d93d1c422c406
Nick Zadrozny <nick@beyondthepath.com>
2011-01-30 12:43:23 -0700

    A few more path tweaks.

--- 01_path.sh
+++ 01_path.sh
@@ -1,5 +1,8 @@
 # I want a custom ~/.bin and also local rubygems
 path=($HOME/.bin $HOME/.gem/ruby/1.8/bin /usr/local/bin $path)
 
+# The basic manpath
+manpath=(/sw/share/man /usr/man /usr/X11R6/man /usr/local/man:/usr/share/man /usr/lib/perl5/man)
+
 # Allow MacPorts man pages
 manpath=($manpath /opt/local/man)
