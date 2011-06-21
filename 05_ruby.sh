2dfd1f11e18a39bc2c8b0160e267b90bd4d2ae98
Nick Zadrozny <nick@beyondthepath.com>
2011-06-21 01:28:10 -0400

    Do bundler things and rvm things.

--- 05_ruby.sh
+++ 05_ruby.sh
@@ -4,9 +4,27 @@ function druby() {
   ruby -rubygems -I lib $@
 }
 
+alias bi='bundle install --path vendor/bundle'
+alias be='bundle exec $*'
 
 # export GEM_HOME="$(brew --prefix)/Cellar/gems/1.9.1"
 export GEM_HOME="/usr/local/Cellar/Gems"
 export RUBYOPT=rubygems
 
-if [[ -s /Users/nick/.rvm/scripts/rvm ]] ; then source /Users/nick/.rvm/scripts/rvm ; fi
+export RBXOPT=-Xrbc.db=/tmp/rbx
+
+[[ -s "/Users/nick/Developer/.rvm/scripts/rvm" ]] && source "/Users/nick/Developer/.rvm/scripts/rvm"
+
+
+function rvmall() {
+  for i in system 1.8.7 1.9.2 rbx; do
+    rvm use $i
+    if [ -f Gemfile ]; then
+      bundle exec $*
+    else
+      $*
+    fi
+  done
+}
+
+alias rvmbi='rvmall bundle install --path vendor/bundle'
