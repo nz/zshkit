d77dbfa4045639bc43df86e57525c7f24d8012d7
Nick Zadrozny <nick@beyondthepath.com>
2011-06-21 01:27:55 -0400

    Cool new truncated prompt hacked together with @tov

--- 10_prompt.sh
+++ 10_prompt.sh
@@ -3,7 +3,13 @@ setopt prompt_subst
 PS_ERROR="%(?::%{$fg_bold[white]$bg[red]%}[%?]%{$reset_color%} )"
 PS_USER="%(#:%{$fg[red]%}:%{$fg[cyan]%})%n%{$reset_color%}"
 PS_HOST="@%{$fg[cyan]%}%m%{$reset_color%}"
-PS_DIR=":%{$fg[cyan]%}%~%{$reset_color%}"
+
+function prompt_dir () {
+  echo -n ":%{$fg[cyan]"
+  print -nP "%~" | ruby -p -e 'n = 3; l = 3; a = $_.split(/\//); $_ = (a[0..(-n - 1)].map{|s|s.length > l ? "#{s[0,l]}…" : s} + a[(-n)..-1]).join("'$fg[blue]'/'$fg[cyan]'") if a.length > n'
+  echo -n "%{$reset_color"
+}
+
 PS_ROOT_HASH="%{[0m%}%(#:%{[0;31m%}#%{[0m%}:)"
 
 PROMPT='\
@@ -11,8 +17,7 @@ PROMPT='\
 $PS_ERROR\
 $PS_USER\
 $PS_HOST\
-$PS_DIR\
-$PS_GIT\
+$(prompt_dir)\
 $(git-prompt)\
 $PS_ROOT_HASH\
  '
