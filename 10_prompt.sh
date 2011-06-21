ad10a38d5cd2b96d857de3df577f2f0c7d5ad8ed
Nick Zadrozny <nick@beyondthepath.com>
2011-06-21 13:27:40 -0400

    Ported back to sed again by @tov

--- 10_prompt.sh
+++ 10_prompt.sh
@@ -4,10 +4,21 @@ PS_ERROR="%(?::%{$fg_bold[white]$bg[red]%}[%?]%{$reset_color%} )"
 PS_USER="%(#:%{$fg[red]%}:%{$fg[cyan]%})%n%{$reset_color%}"
 PS_HOST="@%{$fg[cyan]%}%m%{$reset_color%}"
 
-function prompt_dir () {
-  echo -n ":%{$fg[cyan]"
-  print -nP "%~" | ruby -p -e 'n = 3; l = 3; a = $_.split(/\//); $_ = (a[0..(-n - 1)].map{|s|s.length > l ? "#{s[0,l]}…" : s} + a[(-n)..-1]).join("'$fg[blue]'/'$fg[cyan]'") if a.length > n'
-  echo -n "%{$reset_color"
+
+export prompt_path_max=20
+export prompt_path_segment=3
+
+prompt_dir () {
+    print -nP ":%~" | sed '
+        :loop
+        /.\{'$prompt_path_max'\}/{
+            s@\(/[^/]\{'$prompt_path_segment'\}\)[^/]\{1,\}[^\\]/@\1\\/@
+            tloop
+            s@\(^[^/]\{'$prompt_path_segment'\}\)[^/]\{1,\}[^\\]/@\1\\/@
+        }
+        s@\\/@…/@g
+        s@\([~/]\{1,\}\)@'"$fg[blue]\1$fg[cyan]"'@g
+    '
 }
 
 PS_ROOT_HASH="%{[0m%}%(#:%{[0;31m%}#%{[0m%}:)"
