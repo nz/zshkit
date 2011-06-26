e1402154984a987776fa0549ca1ca1bd56bb5112
Nick Zadrozny <nick@beyondthepath.com>
2011-06-25 21:30:29 -0700

    Extract the sed stuff into its own file. Simplify the coloring.

--- 10_prompt.sh
+++ 10_prompt.sh
@@ -5,26 +5,8 @@ PS_USER="%(#:%{$fg[red]%}:%{$fg[cyan]%})%n%{$reset_color%}"
 PS_HOST="@%{$fg[cyan]%}%m%{$reset_color%}"
 PS_ROOT_HASH="%{[0m%}%(#:%{[0;31m%}#%{[0m%}:)"
 
-export prompt_path_max=20
-export prompt_path_segment=3
-export prompt_path_keep=1
-
 prompt_dir () {
-   local max=$prompt_path_max
-   local segment=$[ prompt_path_segment - 1 ]
-   local keep=$prompt_path_keep
-   print -nP ':%~' | sed '
-       :while
-       /^.\{'$max'\}/{
-           s@\(/[^/]\{'$segment'\}\)[^/][^/]*[^\\]\(\(/.*\)\{'$keep',\}\)$@\1\\○\2@
-           twhile
-           s@\(^[^/]\{'$segment'\}\)[^/][^/]*[^\\]\(\(/.*\)\{'$keep',\}\)$@\1\\○\2@
-       }
-       s@\\/@…/@g
-       s@\\$@…@
-       s@/@'"%{$fg[blue]%}/%{$fg[cyan]%}"'@g
-       s@^:\([~/]*\)@:'"%{$fg[blue]%}\1%{$fg[cyan]%}"'@
-   '
+   print -nP ":%{$fg[cyan]%}%~%{$reset_color%}" | sed -Ef ~/.zshkit/prompt_dir.sed
 }
 
 # :%{$fg[cyan]%}$(print -nP %~)%{$reset_color%}\
