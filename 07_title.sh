fd163dee0aafa0e0719576d26127dfcb4e497edc
Nick Zadrozny <nick@onemorecloud.com>
2019-08-02 12:09:06 -0500

    don't attempt to set the tab title for a non-interactive shell

--- 07_title.sh
+++ 07_title.sh
@@ -1,8 +1,16 @@
 # Set the terminal title in OS X, stolen from http://tinyurl.com/5u9wfr
 case $TERM in (*xterm*|ansi)
-	function settab { print -Pn "\e]1;%n@%m: %~\a" }
-	function settitle { print -Pn "\e]2;%n@%m: %~\a" }
-	function chpwd { settab; settitle }
-	settab; settitle
+	function settab {
+		print -Pn "\e]1;%n@%m: %~\a"
+	}
+	function settitle {
+		print -Pn "\e]2;%n@%m: %~\a"
+	}
+	function chpwd {
+		settab; settitle
+	}
+	if [[ -o interactive ]]; then
+		settab; settitle
+	fi
 	;;
 esac
