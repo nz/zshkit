3311d44e4f618f6eef066b5e4b9a1a5152231e78
Nick Zadrozny <nick@beyondthepath.com>
2019-08-05 10:50:57 -0500

    look in project node_modules for binaries

--- 01_path.sh
+++ 01_path.sh
@@ -1,5 +1,5 @@
 # I want a custom ~/.bin and also local rubygems
-path=(./bin $HOME/.bin /usr/local/bin $path)
+path=(./bin ./node_modules/.bin $HOME/.bin /usr/local/bin $path)
 
 # The basic manpath
 manpath=(/sw/share/man /usr/man /usr/X11R6/man /usr/local/man:/usr/share/man /usr/lib/perl5/man)
